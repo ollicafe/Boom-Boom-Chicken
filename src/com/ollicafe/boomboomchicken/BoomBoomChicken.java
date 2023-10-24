507c65f0517a4396b6151ccbd1af515ea92fe017
olli <olli@ollicafe.com>
2023-10-24 10:48:11 -0700

    creation

---
+++ src/com/ollicafe/boomboomchicken/BoomBoomChicken.java
@@ -0,0 +1,17 @@
+package com.ollicafe.boomboomchicken;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BoomBoomChicken extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
